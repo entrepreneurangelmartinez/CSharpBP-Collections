464b5c111009e4c5b02e760899521d494bc90378
entrepreneurangelmartinez <entrepreneurangelmartinez@gmail.com>
2016-10-15 13:48:09 -0500

    Acme.Biz repair

---
+++ Acme.Biz/Acme.Biz/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    public class Product
+    {
+        public Product()
+        {
+            var colorOptions = new string[4];
+            colorOptions[0] = "Red";
+            colorOptions[1] = "Expresso";
+            colorOptions[2] = "White";
+            colorOptions[3] = "Navy";
+            Console.WriteLine(colorOptions);
+        }
+    }
+}
